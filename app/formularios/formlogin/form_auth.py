947b54d419de0c3a2adba074eb7bd852b61f030e
Jaime <navarrocruzjaime@gmail.com>
2025-09-12 10:23:53 -0500

    Eliminada la carpeta app del indice git

--- app/formularios/formlogin/form_auth.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-	usuario=StringField('Usuario',validators=[DataRequired()])
-	clave=PasswordField('Contraseña',validators=[DataRequired()])
-	recordar=BooleanField('Recordar')
-	btnInicio=SubmitField('Iniciar')
